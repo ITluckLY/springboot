e8f6cac9b9f1d052d38fb888d1817bcdd77174bd
lyy <183938622@qq.com>
2020-06-07 15:14:02 +0800

    添加swagger api

--- src/main/java/com/springboot_c/controller/HelloController.java
+++ src/main/java/com/springboot_c/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.springboot_c.controller;
 
 import com.springboot_c.pojo.User;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-  @RequestMapping("/hello")
+  @GetMapping("/hello")
   public String   hello(){
     return "hello";
   }
 
-  @RequestMapping("/login")
-  public String login(){
+  @ApiOperation("login 控制")
+  @GetMapping("/login")
+  public String login(@ApiParam("用户名") String username){
 
-    return null;
+    return "hello"+username;
   }
 
 
